7b43444164c443f8ff5e9552d2d99e3c07ffcb76
Mauricio Dos Santos Henrique <mauricio.henrique@somosphi.com>
2022-04-13 17:27:39 -0300

    Created package to save http decorators

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+const create_route_decorator = (method: string) =>
+  function (path: string, middlewares: Function[] = []): Function {
+    return function (constructor: Function) {
+      const { status_code, schema, has_schema } = constructor.prototype;
+      if (!constructor.prototype.route_configs) {
+        constructor.prototype.route_configs = [
+          {
+            path,
+            method,
+            middlewares,
+            status_code,
+            schema,
+            has_schema,
+          },
+        ];
+      }
+    };
+  };
+
+export const get = create_route_decorator("get");
+
+export const post = create_route_decorator("post");
+
+export const put = create_route_decorator("put");
+
+export const patch = create_route_decorator("patch");
+
+export const del = create_route_decorator("delete");
+
+export const http_status = function (status_code: number) {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    target.status_code = status_code;
+  };
+};
+
+export const version = function (version_param: string): Function {
+  return function (constructor: Function) {
+    const { route_configs } = constructor.prototype;
+    for (const iterator of route_configs) {
+      if (version_param) {
+        if (version_param.indexOf("/") > -1) {
+          iterator.path = version_param + iterator.path;
+        } else {
+          throw new Error('Decorator version need "/" to work.');
+        }
+      }
+    }
+  };
+};
